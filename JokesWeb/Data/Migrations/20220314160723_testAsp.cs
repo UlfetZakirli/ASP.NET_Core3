5346e53e46aa6c8b95babc053b471af7b2cf1133
UlfetZakirli <ulfetzakirli@gmail.com>
2022-03-14 20:40:42 +0400

    Add project files.

---
+++ JokesWeb/Data/Migrations/20220314160723_testAsp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JokesWeb.Data.Migrations
+{
+    public partial class testAsp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
